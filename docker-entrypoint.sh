b09026cdae3a20c0126303ce74411d5a7ec713a7
Edu Herraiz Aparicio <gshark@gmail.com>
2016-11-09 20:11:23 +0100

    [wip] Fixing wal-e recovery and restructure start init

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -24,6 +24,45 @@ function configure_wale() {
     chmod 755 -R /etc/wal-e.d
 }
 
+function configure_pg_hba() {
+    # check password first so we can output the warning before postgres
+    # messes it up
+    if [ "$POSTGRES_PASSWORD" ]; then
+            pass="PASSWORD '$POSTGRES_PASSWORD'"
+        authMethod=md5  
+    else
+        # The - option suppresses leading tabs but *not* spaces. :)
+        cat >&2 <<-'EOWARN'
+                                ****************************************************
+                                WARNING: No password has been set for the database.
+                                         This will allow anyone with access to the
+                                         Postgres port to access your database. In
+                                         Docker's default configuration, this is
+                                         effectively any other container on the same
+                                         system.
+
+                                         Use "-e POSTGRES_PASSWORD=password" to set
+                                         it in "docker run".
+                                ****************************************************
+EOWARN
+
+        pass=
+        authMethod=trust
+    fi
+                
+    cat <<EOPGHBA > "$PGDATA/pg_hba.conf"
+# "local" is for Unix domain socket connections only
+local   all             all                                     trust
+# IPv4 local connections:
+host    all             all             127.0.0.1/32            trust
+# IPv6 local connections:
+host    all             all             ::1/128                 trust
+EOPGHBA
+
+    { echo; echo "host all all 0.0.0.0/0 $authMethod"; } >> "$PGDATA/pg_hba.conf"
+    chown postgres "$PGDATA/pg_hba.conf"
+}
+
 if [ "$1" = 'postgres' ]; then
         mkdir -p "$PGDATA"
         chmod 700 "$PGDATA"
@@ -51,82 +90,24 @@ if [ "$1" = 'postgres' ]; then
                 
                 echo "restore_command = 'envdir /etc/wal-e.d/env_recover wal-e wal-fetch \"%f\" \"%p\"'" > $PGDATA/recovery.conf
                 chown postgres:postgres "$PGDATA/recovery.conf"
-                
                 chmod 700 -R $PGDATA
-                
-                # check password first so we can output the warning before postgres
-                # messes it up
-                if [ "$POSTGRES_PASSWORD" ]; then
-                        pass="PASSWORD '$POSTGRES_PASSWORD'"
-                        authMethod=md5  
-                else
-                        # The - option suppresses leading tabs but *not* spaces. :)
-                        cat >&2 <<-'EOWARN'
-                                ****************************************************
-                                WARNING: No password has been set for the database.
-                                         This will allow anyone with access to the
-                                         Postgres port to access your database. In
-                                         Docker's default configuration, this is
-                                         effectively any other container on the same
-                                         system.
-
-                                         Use "-e POSTGRES_PASSWORD=password" to set
-                                         it in "docker run".
-                                ****************************************************
-EOWARN
-
-                        pass=
-                        authMethod=trust
-                fi
-                
-                cat <<EOPGHBA > "$PGDATA/pg_hba.conf"
-# "local" is for Unix domain socket connections only
-local   all             all                                     trust
-# IPv4 local connections:
-host    all             all             127.0.0.1/32            trust
-# IPv6 local connections:
-host    all             all             ::1/128                 trust
-EOPGHBA
-
-               { echo; echo "host all all 0.0.0.0/0 $authMethod"; } >> "$PGDATA/pg_hba.conf"
-                chown postgres "$PGDATA/pg_hba.conf"
                                
             else
+                echo 'Initiating PostgreSQL Database.'
                 gosu postgres initdb
+            fi
+                
+            configure_pg_hba
 
-                # check password first so we can output the warning before postgres
-                # messes it up
-                if [ "$POSTGRES_PASSWORD" ]; then
-                        pass="PASSWORD '$POSTGRES_PASSWORD'"
-                        authMethod=md5  
-                else
-                        # The - option suppresses leading tabs but *not* spaces. :)
-                        cat >&2 <<-'EOWARN'
-                                ****************************************************
-                                WARNING: No password has been set for the database.
-                                         This will allow anyone with access to the
-                                         Postgres port to access your database. In
-                                         Docker's default configuration, this is
-                                         effectively any other container on the same
-                                         system.
+            # internal start of server in order to allow set-up using psql-client           
+            # does not listen on TCP/IP and waits until start finishes
+            gosu postgres pg_ctl -D "$PGDATA" \
+                    -o "-c listen_addresses='' -c config_file='/etc/postgres/postgresql.conf'" \
+                    -w start
 
-                                         Use "-e POSTGRES_PASSWORD=password" to set
-                                         it in "docker run".
-                                ****************************************************
-EOWARN
-
-                        pass=
-                        authMethod=trust
-                fi
-
-                { echo; echo "host all all 0.0.0.0/0 $authMethod"; } >> "$PGDATA/pg_hba.conf"
-
-                # internal start of server in order to allow set-up using psql-client           
-                # does not listen on TCP/IP and waits until start finishes
-                gosu postgres pg_ctl -D "$PGDATA" \
-                        -o "-c listen_addresses=''" \
-                        -w start
-
+            if [ "$RECOVER_WALE" = 'True' ]; then
+                echo "Nothing to do, just wait..."
+            else
                 : ${POSTGRES_USER:=postgres}
                 : ${POSTGRES_DB:=$POSTGRES_USER}
                 export POSTGRES_USER POSTGRES_DB
@@ -162,19 +143,19 @@ EOSQL
                         esac
                         echo
                 done
-
-                gosu postgres pg_ctl -D "$PGDATA" -m fast -w stop
-
-                echo
-                echo 'PostgreSQL init process complete; ready for start up.'
-                echo
             fi
+
+            gosu postgres pg_ctl -D "$PGDATA" -m fast -w stop
+
+            echo
+            echo 'PostgreSQL init process complete; ready for start up.'
+            echo
         fi
 
         echo
         echo 'Initiating PostgreSQL'
         echo
-        gosu postgres "$@" -c config_file=/etc/postgres/postgresql.conf
+        exec gosu postgres "$@" -c config_file=/etc/postgres/postgresql.conf
 
 
 elif [ "$1" = 'cron' ]; then
